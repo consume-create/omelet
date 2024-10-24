3ed6e3227ecb2be4171e862f0c1c4f8226d1eb55
Hotlines™ <connect@hotlines.studio>
2024-10-24 16:27:46 -0600

    nuxt config update

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,28 +1,41 @@
-// require("dotenv").config({ path: "../sanity/.env" });
-// import { createClient } from "@nuxtjs/sanity";
+// require('dotenv').config({ path: '../sanity/.env' });
+// import { createClient } from '@nuxtjs/sanity';
 //
 // const client = createClient({
 //   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-//   dataset: "production",
+//   dataset: 'production',
 //   useCdn: false,
 //   minimal: true
 // })
 
-const site_name = "Omelet | Los Angeles Ad Agency";
-const site_description = "We’re an independent creative company that makes impact.";
-const site_url = "https://omelet.com"
+const site_name = 'Omelet | Los Angeles Ad Agency';
+const site_description = 'We’re an independent creative company that makes impact.';
+const site_url = 'https://omelet.com'
 
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
+  //
+  // Runtime config
+  //
+  runtimeConfig: {
+    public: {
+      isDev: process.env.NODE_ENV === 'development'
+    }
+  },
   //
   // SSR + Target
   //
   ssr: true,
-  target: "static",
+  target: 'static',
   //
   // Sourcemap https://nuxtseo.com/sitemap/getting-started/installation
   //
+  runtimeConfig: {
+    public: {
+      isDev: process.env.NODE_ENV === 'development'
+    }
+  },
   sourcemap: {
     server: true,
     client: false
@@ -39,27 +52,30 @@ export default defineNuxtConfig({
   //
   app: {
     head: {
+      htmlAttrs: {
+        lang: 'en'
+      },
       title: site_name,
       meta: [
-        { charset: "utf-8" },
-        { name: "viewport", content: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" },
-        { property: "og:type", content: "website" },
-        { property: "og:site_name", content: site_name },
-        { hid: "description", name: "description", content: site_description },
-        { hid: "og:title", property: "og:title", content: site_name },
-        { hid: "og:description", property: "og:description", content: site_description },
-        { hid: "og:url", property: "og:url", content: site_url },
-        // { hid: "og:image", property: "og:image", content: `${site_url}/images/...` },
-        { name: "twitter:card", content: "summary" },
-        { name: "msapplication-TileColor", content:"#ffffff" },
-        { name: "theme-color", content:"#ffffff" }
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: site_name },
+        { hid: 'description', name: 'description', content: site_description },
+        { hid: 'og:title', property: 'og:title', content: site_name },
+        { hid: 'og:description', property: 'og:description', content: site_description },
+        { hid: 'og:url', property: 'og:url', content: site_url },
+        // { hid: 'og:image', property: 'og:image', content: `${site_url}/images/...` },
+        { name: 'twitter:card', content: 'summary' },
+        { name: 'msapplication-TileColor', content:'#ffffff' },
+        { name: 'theme-color', content:'#ffffff' }
       ],
       link: [
-        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
-        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
-        { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png" },
-        { rel: "mask-icon", href: "/safari-pinned-tab.svg", color: "#000000" },
-        { rel: "manifest", href: "/site.webmanifest" }
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#000000' },
+        { rel: 'manifest', href: '/site.webmanifest' }
       ]
     }
   },
